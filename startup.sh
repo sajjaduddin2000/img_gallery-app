396af12b140ca956d25e03423fd3af9044273e1a
sajjaduddin2000 <sajjaduddin2000@outlook.com>
2025-03-15 00:01:08 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-apt-get update && apt-get install -y cifs-utils
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
+sudo apt-get update && sudo apt-get install -y cifs-utils
